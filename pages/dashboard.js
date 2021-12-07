b402bb6e94314a7f15cbfe18a535f8c3dc1fd100
Andrey SHevkenov <shevkenov@gmail.com>
2021-12-07 18:19:50 +0200

    add pages: dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,17 +1,29 @@
 import React from "react";
+import useSWR from "swr";
+
 import DashboardShell from "../compnents/DashboardShell";
 import EmptyState from "../compnents/EmptyState";
 import FreePlanEmptyState from "../compnents/FreePlanEmptyState";
+import SitesTable from "../compnents/SitesTable";
+import LoadingSkeleton from "../compnents/SkeletonLoading";
 import { useAuth } from "../lib/auth";
+import fetcher from '../utils/fetcher';
 
 const Dashboard = () => {
   const auth = useAuth();
+  const {data, error} = useSWR('/api/sites', fetcher);
 
-  if (!auth.user) return "Loading ......";
+  if(!data) return (
+      <DashboardShell>
+          <LoadingSkeleton />
+      </DashboardShell>
+  )
 
   return (
     <DashboardShell>
-      <EmptyState /> :
+        {
+            !data.length ? <EmptyState /> : <SitesTable sites={data}/>
+        }
     </DashboardShell>
   );
 };
