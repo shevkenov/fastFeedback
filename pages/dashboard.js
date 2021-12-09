0aad7f1a13ce0a73d219a343b1a258f8a428cb87
Andrey SHevkenov <shevkenov@gmail.com>
2021-12-09 17:49:41 +0200

    add: logout button

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -6,11 +6,9 @@ import EmptyState from "../compnents/EmptyState";
 import FreePlanEmptyState from "../compnents/FreePlanEmptyState";
 import SitesTable from "../compnents/SitesTable";
 import LoadingSkeleton from "../compnents/SkeletonLoading";
-import { useAuth } from "../lib/auth";
 import fetcher from '../utils/fetcher';
 
 const Dashboard = () => {
-  const auth = useAuth();
   const {data, error} = useSWR('/api/sites', fetcher);
 
   if(!data) return (
