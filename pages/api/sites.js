2ea28c659f28d4125b73973af85d5c35c9a363e8
Andrey SHevkenov <shevkenov@gmail.com>
2021-12-16 11:26:52 +0200

    change: fetcher func

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -5,7 +5,8 @@ export default async function handler(req, res) {
   const {token} = req.headers;
   
   const {user_id} = await auth.verifyIdToken(token);
-  const sites = await getUserSites(user_id);
+  //const sites = await getUserSites(user_id);
+  const sites = await getSites();
   
   res.status(200).json(sites);
 }
