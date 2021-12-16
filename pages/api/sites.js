d878565fa2eed39c6d4e92bd1ce04f02dfb27eca
Andrey SHevkenov <shevkenov@gmail.com>
2021-12-16 12:07:43 +0200

    change: fetcher func

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,10 +1,10 @@
-import { auth } from "../../lib/firabase-admin";
+//import { auth } from "../../lib/firabase-admin";
 import { getUserSites, getSites } from "../../lib/db";
 
 export default async function handler(req, res) {
   const {token} = req.headers;
   
-  const {user_id} = await auth.verifyIdToken(token);
+  //const {user_id} = await auth.verifyIdToken(token);
   //const sites = await getUserSites(user_id);
   const sites = await getSites();
   
