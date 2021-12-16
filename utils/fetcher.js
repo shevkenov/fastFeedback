cb4a07697b38aaaac02f6ce429b372ceb4099d11
Andrey SHevkenov <shevkenov@gmail.com>
2021-12-16 12:35:34 +0200

    change: fetcher func

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,5 +1,14 @@
-async function fetcher(url){
-    const parsedData = await fetch(url).then(data => data.json());
+async function fetcher(url, {token}){
+    const header = {
+        "Content-Type": "application/json",
+        token
+    }
+    
+    const parsedData = await fetch(url, {
+        method: "GET",
+        headers: new Headers(header),
+        credentials: 'same-origin'
+    }).then(data => data.json());
 
     return parsedData;
 }
